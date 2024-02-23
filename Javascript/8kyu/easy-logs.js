98d5b0488edb98d85301f3faaefe9f3253082519
“ZigurdsKomarovs” <komarovs.zigurds@gmail.com>
2024-02-23 21:23:51 +0200

    logs

---
+++ Javascript/8kyu/easy-logs.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: LOGxA + LOGxB
+
+//Solutions
+function logs(x , a, b){
+    return (Math.log(a*b) / Math.log(x));
+    }
